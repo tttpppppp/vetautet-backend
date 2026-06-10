0fcf1a6fa5b51fd8f89b9213e8763d7c1e085adb
Trần Tiến Phúc <112532197+tttpppppp@users.noreply.github.com>
2026-06-11 06:19:24 +0700

    Speed up backend startup on Render

--- vetautet-start/src/main/java/com/vetautet/start/StartApplication.java
+++ vetautet-start/src/main/java/com/vetautet/start/StartApplication.java
@@ -6,13 +6,17 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.config.BootstrapMode;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
 @ComponentScan(basePackages = {"com.vetautet"})
-@EnableJpaRepositories(basePackages = "com.vetautet.infrastructure.persistence.repository")
+@EnableJpaRepositories(
+        basePackages = "com.vetautet.infrastructure.persistence.repository",
+        bootstrapMode = BootstrapMode.LAZY
+)
 @EntityScan(basePackages = "com.vetautet.infrastructure.persistence.entity")
 public class StartApplication {
     public static void main(String[] args) {
